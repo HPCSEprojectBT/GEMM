829bc73e30ada75f4d38c5c5d5bc767c710956ee
Benjamin <ulmerb@student.ethz.ch>
2014-05-16 00:37:22 +0200

    diffusion less buggy :-)

--- Diffeq/diffusion.cpp
+++ Diffeq/diffusion.cpp
@@ -6,11 +6,8 @@
 
 using namespace std;
 
-const int t_max = 10000;
-double dt = 0.0001;
-double dx = 0.1;
-double dy = 0.1;
-double dz = 0.1;
+const int t_max = 100000;
+double dt = 0.00001;
 
 void print_matrices(matrix<double> & m, int size, int rank)
 {
@@ -28,14 +25,14 @@ int main (int argc, char **argv)
 	//-------------driver params----------------------------
 	int world_dim=3;
 	int dims[3]={0,0,0};
-	int N = 4; //target global number of DOFS in each direction
+	int N = 10; //target global number of DOFS in each direction
 	int N_new; //global number of DOFS in each direction after rounding 
 	double L = 1.; //edgelength of simulated cube
 	vector<double> dx(3,0); //discrete spacing in each direction
 	vector<double> n(3,0); //local number of DOFS in x,y,z direction
 	int local_dofs; //number of local dofs per matrix
-	double init_heat = 10000000000;
-	int print = 100;
+	double init_heat = 77777777;
+	int print = 1000;
 	//--------------------------------------------------------
 
 	//MPI Init and CartComm creation
@@ -80,21 +77,22 @@ int main (int argc, char **argv)
 	matrix<double> u(n[0]+2,n[1]+2,n[2]+2); //local matrix including ghost plains
 	matrix<double> u_old(n[0]+2,n[1]+2,n[2]+2);//local matrix including ghost plains
 
-	//generate initial local matrizes 
-	for (int k=0; k<n[2]; ++k) {
-		for (int j=0; j<n[1]; ++j) {
-			for (int i=0; i<n[0]; ++i) {
-				if (rank == 14 && i==1 && j ==1 && k==1) u(i,j,k) = init_heat; //inital heat field
+	//generate initial local matrizes without ghost cells
+	for (int k=1; k<=n[2]; ++k) {
+		for (int j=1; j<=n[1]; ++j) {
+			for (int i=1; i<=n[0]; ++i) {
+				if (rank == 0) u(i,j,k) = init_heat; //inital heat field
 				else u(i,j,k) = 0;
 			}
 		}
 	}
 
 	//print initial matrix settup
-	/*	if (rank==0) cout << "Initial Matrices :" << endl;
-		print_matrices(u, size, rank);
-		MPI_Barrier( MPI_COMM_WORLD );
-	 */
+	if (rank==0) cout << "Initial Matrices :" << endl;
+	print_matrices(u, size, rank);
+
+	MPI_Barrier( MPI_COMM_WORLD );
+
 
 	MPI_Barrier( MPI_COMM_WORLD );
 	if(rank==0) cout << "local Matrix creation done" << endl;
@@ -123,8 +121,8 @@ int main (int argc, char **argv)
 
 
 	//BT send/recv
-	int bt_tag = 12;
-	int tb_tag = 21;
+	int bt_tag = 13;
+	int tb_tag = 31;
 	MPI_Datatype BT_TYPE;
 
 	MPI_Type_contiguous(u.num_xy(),MPI_DOUBLE, &BT_TYPE); //prepare type for bottom/top sends
@@ -141,8 +139,8 @@ int main (int argc, char **argv)
 			Comm_Cart, &status);
 
 	//FB send/recv
-	int fb_tag = 12;
-	int bf_tag = 21;
+	int fb_tag = 14;
+	int bf_tag = 41;
 	MPI_Datatype FB_TYPE;
 
 	MPI_Type_vector(u.num_z(),u.blocklength_xz(),u.stride_xz(), MPI_DOUBLE, &FB_TYPE); //prepare type for front/back sends
@@ -160,6 +158,15 @@ int main (int argc, char **argv)
 	MPI_Sendrecv(&u.data_[u.num_xy()-2*u.num_x()],1,FB_TYPE,back,fb_tag,
 			&u.data_[0],1,FB_TYPE,front,fb_tag,
 			Comm_Cart, &status);
+
+	u_old = u;
+
+	if (rank==0) cout << "Initial Matrices after ghost cell shifting :" << endl;
+	print_matrices(u, size, rank);
+	//print_matrices(u_old, size, rank);
+
+
+
 	//---------------------------------------------END OF SENDING AND RECIEVING---------------------------------------------------------------
 	//------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -198,23 +205,10 @@ int main (int argc, char **argv)
 	//------------------------START TIMESTEPPING -----------------------------------------------
 	//------------------------------------------------------------------------------------------
 
+	int print_counter = 0;
+
 	for (int t=0; t<t_max; ++t){
 
-		//update local matrix
-		double f =0;
-		for(double i=1; i<=(n[0]); i++){
-			for(double j=1;j<=(n[1]); j++){
-				for(double k=1; k<=(n[2]); k++){
-					f=1./dx[0]*(-2.*u(i,j,k)+u(i+1,j,k)+u(i-1,j,k));
-					f+=1./dx[1]*(-2.*u(i,j,k)+u(i,j+1,k)+u(i,j-1,k));
-					f+=1./dx[2]*(-2.*u(i,j,k)+u(i,j,k+1)+u(i,j,k+1));
-					u_old(i,j,k)=u(i,j,k)+dt*f;						
-				}
-			}
-		}
-
-		swap(u,u_old);
-		int print_counter = 0;
 		if(t<10 || t%print==0){
 
 			//compute global heat values for output
@@ -226,6 +220,7 @@ int main (int argc, char **argv)
 				for(double j=1;j<=(n[1]); j++){
 					for(double k=1; k<=(n[2]); k++){
 						lfvals[cnt] = u(i,j,k);
+						//cout << lfvals[cnt] << endl;
 						cnt++;
 					}
 				}
@@ -255,7 +250,86 @@ int main (int argc, char **argv)
 			if (rank ==0) write_3d(dxvals, dyvals, dzvals, fvals, print_counter);
 			print_counter++;
 		}
+		MPI_Barrier( MPI_COMM_WORLD );
 
+
+		//update local matrix (timestep)---------------------------
+		//if(rank==0) cout << "compute timestep update: " << t << endl;
+		double f = 0;
+		//cout << "i am rank " << rank << "with f=" << f << endl;
+		for(double i=1; i<=(n[0]); i++){
+			for(double j=1;j<=(n[1]); j++){
+				for(double k=1; k<=(n[2]); k++){
+					f=1./dx[0]*(-2.*u(i,j,k)+u(i+1,j,k)+u(i-1,j,k));
+					f+=1./dx[1]*(-2.*u(i,j,k)+u(i,j+1,k)+u(i,j-1,k));
+					f+=1./dx[2]*(-2.*u(i,j,k)+u(i,j,k+1)+u(i,j,k+1));
+					//cout << "f = " << f << endl; 
+					u_old(i,j,k)=u(i,j,k)+dt*f;						
+				}
+			}
+		}
+		swap(u,u_old);
+
+		//send ghost cells arround
+		//---------------------------------------------------------------------------------------------------
+		//send left and right
+		int lr_tag = 12;
+		int rl_tag = 21;
+		MPI_Datatype LR_TYPE;
+
+		//LR send/recv
+		MPI_Type_vector(u.num_yz(),u.blocklength_yz(),u.stride_yz(), MPI_DOUBLE, &LR_TYPE); //prepare type for left/right sends
+		MPI_Type_commit(&LR_TYPE);
+
+		// Send to the left, recieve from the right
+		MPI_Sendrecv(&u.data_[1],1,LR_TYPE,left,lr_tag,
+				&u.data_[u.num_x()-1],1,LR_TYPE,right,lr_tag,
+				Comm_Cart, &status);
+
+		// Send to the right, recieve from the left
+		MPI_Sendrecv(&u.data_[u.num_x()-2],1,LR_TYPE,right,rl_tag,
+				&u.data_[0],1,LR_TYPE,left,rl_tag,
+				Comm_Cart, &status);
+
+
+		//BT send/recv
+		int bt_tag = 13;
+		int tb_tag = 31;
+		MPI_Datatype BT_TYPE;
+
+		MPI_Type_contiguous(u.num_xy(),MPI_DOUBLE, &BT_TYPE); //prepare type for bottom/top sends
+		MPI_Type_commit(&BT_TYPE);
+
+		// Send to top, recieve from bottom
+		MPI_Sendrecv(&u.data_[u.num_xy()],1,BT_TYPE,top,bt_tag,
+				&u.data_[u.num_xy()*(u.num_z()-1)],1,BT_TYPE,bottom,bt_tag,
+				Comm_Cart, &status);
+
+		// Send to bottom, recieve from top
+		MPI_Sendrecv(&u.data_[u.num_xy()*(u.num_z()-2)],1,BT_TYPE,bottom,tb_tag,
+				&u.data_[0],1,BT_TYPE,top,tb_tag,
+				Comm_Cart, &status);
+
+		//FB send/recv
+		int fb_tag = 14;
+		int bf_tag = 41;
+		MPI_Datatype FB_TYPE;
+
+		MPI_Type_vector(u.num_z(),u.blocklength_xz(),u.stride_xz(), MPI_DOUBLE, &FB_TYPE); //prepare type for front/back sends
+
+		MPI_Type_commit(&FB_TYPE);
+
+		// Send to front, recieve from back
+
+		MPI_Sendrecv(&u.data_[u.num_x()],1,FB_TYPE,front,fb_tag,
+				&u.data_[u.num_xy()-u.num_x()],1,FB_TYPE,back,fb_tag,
+				Comm_Cart, &status);
+
+		// Send to back, recieve from front
+
+		MPI_Sendrecv(&u.data_[u.num_xy()-2*u.num_x()],1,FB_TYPE,back,fb_tag,
+				&u.data_[0],1,FB_TYPE,front,fb_tag,
+				Comm_Cart, &status);
 	}
 	MPI_Finalize();
 
