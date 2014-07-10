95c1acae0c05d1e11282a9f0cfe3bf7408209037
Tobias W <tobias.wicky@hotmail.com>
2014-07-10 17:27:43 +0200

    afe

---
+++ Gemm_9_7/scripts/run_ws.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm *.csv
+sbatch ./WS/batch_ws_1.txt
+sbatch ./WS/batch_ws_4.txt
+sbatch ./WS/batch_ws_9.txt
+sbatch ./WS/batch_ws_16.txt
+sbatch ./WS/batch_ws_25.txt
+sbatch ./WS/batch_ws_36.txt
+sbatch ./WS/batch_ws_49.txt
+sbatch ./WS/batch_ws_64.txt
+sbatch ./WS/batch_ws_81.txt
+sbatch ./WS/batch_ws_100.txt
+sbatch ./WS/batch_ws_144.txt
+sbatch ./WS/batch_ws_196.txt
+sbatch ./WS/batch_ws_225.txt
+sbatch ./WS/batch_ws_256.txt
